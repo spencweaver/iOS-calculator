95b4ce24f77fbd52de02ce55adb0c19a87001b1d
Spencer Weaver <spencweaver@gmail.com>
2024-04-29 08:46:36 -0400

    update to conform with MVC

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,29 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Spencer Weaver on 4/26/24.
+//  Copyright © 2024 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    var number: Double
+    
+    init(number: Double) {
+        self.number = number
+    }
+    
+    func calculate(symbol: String) -> Double? {
+        if symbol == "+/-" {
+            return number * -1
+        } else if symbol == "AC" {
+            return 0
+        } else if symbol == "%" {
+            return number * 0.01
+        }
+        return nil
+    }
+}
