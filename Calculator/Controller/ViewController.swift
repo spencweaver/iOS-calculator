06071848e8048f21b7d3d0468d96f06a2ddbcbea
Spencer Weaver <spencweaver@gmail.com>
2024-04-30 09:27:24 -0400

    change if statement to switch statement

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -38,10 +38,9 @@ class ViewController: UIViewController {
         if let calcMethod = sender.currentTitle {
             
             
-            guard let result = calculator.calculate(symbol: calcMethod) else {
-                fatalError("The result is nil")
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
             }
-            displayValue = result
         }
     }
 
